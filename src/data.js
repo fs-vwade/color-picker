60f12c33e49c6b6bfe3076f5798ff6b2d2898861
Victor Wade <p2525657888@gmail.com>
2024-09-26 13:19:12 -1000

    created data element with list of colors

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const colors = [
+	{
+		color: "autumn",
+		hex: "#f02800",
+	},
+	{
+		color: "lemon",
+		hex: "#f0b400",
+	},
+	{
+		color: "spearmint",
+		hex: "#00f0b4",
+	},
+	{
+		color: "bubblegum",
+		hex: "#00b4f0",
+	},
+	{
+		color: "rose",
+		hex: "#f00050",
+	},
+];
